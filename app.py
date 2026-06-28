5b29db76e4b26f654aa4a96f4e5333c596db75f4
zhu <1913774864@qq.com>
2026-06-28 20:24:04 +0800

    first commit

--- app.py
+++ app.py
@@ -566,4 +566,4 @@ def workspace_download():
 
 
 if __name__ == "__main__":
-    app.run(host=config.FLASK_HOST, port=config.FLASK_PORT, debug=True)
+    app.run(host=config.FLASK_HOST, port=config.FLASK_PORT, debug=False)

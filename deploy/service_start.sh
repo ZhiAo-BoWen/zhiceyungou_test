d7d8d2154829efe2a28972fefbbb6a78e872b942
zhu <1913774864@qq.com>
2026-07-02 04:41:14 +0800

    first commit

--- deploy/service_start.sh
+++ deploy/service_start.sh
@@ -2,4 +2,4 @@
 
 cd /home/admin/zhiceyungou_test
 
-/home/admin/miniconda3/envs/zhiceyungou_test/bin/gunicorn -b ${FLASK_HOST}:${FLASK_PORT} -w 4 app:app
+/home/admin/miniconda3/envs/zhiceyungou_test/bin/python app.py
